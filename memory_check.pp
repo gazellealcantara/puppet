d7c738719a604b2b4a8c54aac63ac22e74b5ebf7
gazellealcantara <gazelle.alcantara@gmail.com>
2018-05-12 16:39:10 -0400

    Automating memory check

---
+++ memory_check.pp
@@ -0,0 +1,43 @@
+$packages = [ 'yum', 'curl', 'git', 'wget' ]
+package { $packages: ensure => 'installed' }
+
+file { "/home/monitor":
+  ensure => "directory",
+}
+
+user { 'monitor':
+  ensure => present,
+  home => "/home/monitor",
+  shell => "/bin/bash",
+}
+
+file { "/home/monitor/scripts":
+  ensure => "directory",
+}
+
+exec { "git":
+  command => "/usr/bin/wget -q https://rawcdn.githack.com/gazellealcantara/memory-check/master/memory_check.sh -O /home/monitor/scripts/memory_check",
+  path => "/home/monitor/scripts"
+}
+
+file { "/home/monitor/scripts/memory_check":
+  ensure => "present",
+  mode => "0755",
+}
+
+file { "/home/monitor/src":
+  ensure => "directory",
+}
+
+file { "/home/monitor/src/my_memory_check":
+  ensure => 'link',
+  target => "/home/monitor/scripts/memory_check",
+}
+
+cron { "memory check": 
+  command => "/home/monitor/src/my_memory_check",
+  user => monitor,
+  hour => "*",
+  minute => "*/10",
+}
+
